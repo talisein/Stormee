7c590e80fd127885527b0fcab669392905151532
Andrew Potter <agpotter@gmail.com>
2011-03-23 18:37:00 -0700

    Add license and copyright information.

--- src/parse.py
+++ src/parse.py
@@ -1,4 +1,22 @@
 # -*- coding: utf-8 -*-
+#
+#    Copyright (C) 2011 Andrew G. Potter
+#    This file is part of the GNOME Common Alerting Protocol Viewer.
+# 
+#    GNOME Common Alerting Protocol Viewer is free software: you can r
+#    edistribute it and/or modify it under the terms of the GNU General 
+#    Public License as published by the Free Software Foundation, either 
+#    version 3 of the License, or (at your option) any later version.
+# 
+#    GNOME Common Alerting Protocol Viewer is distributed in the hope 
+#    that it will be useful, but WITHOUT ANY WARRANTY; without even the 
+#    implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR 
+#    PURPOSE.  See the    GNU General Public License for more details.
+# 
+#    You should have received a copy of the GNU General Public License
+#    along with GNOME Common Alerting Protocol Viewer.  
+#    If not, see <http://www.gnu.org/licenses/>.
+#===============================================================================
 
 from xml.sax import saxutils
 from xml.sax import make_parser
