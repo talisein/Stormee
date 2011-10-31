eba76e8af769ed4b142185dd69ec0419ee30b6e5
Andrew Potter <agpotter@gmail.com>
2011-10-30 22:33:39 -0700

    Switch main to use Application

--- src/main.cxx
+++ src/main.cxx
@@ -1,21 +1,15 @@
 #include <libintl.h>
 #include <gtkmm/main.h>
-#include "config.h"
-#include "util.hxx"
-#include "main_window.hxx"
-
+#include <glibmm/thread.h>
+#include "application.hxx"
 
 int main (int argc, char *argv[])
 {
-  Gtk::Main kit(argc, argv);
   if(!Glib::thread_supported()) Glib::thread_init();
 
-  auto refBuilder = CAPViewer::Util::getGtkBuilder();
-  CAPViewer::Window *window;
-  refBuilder->get_widget_derived("mainWindow", window);
+  CAPViewer::Application app(argc, argv);
+  app.run();
 
-  kit.run(*window);
-  delete window;
   return EXIT_SUCCESS;
 }
 
